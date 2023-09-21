f7861489c6290219dd4f33c7b598f08119f59119
Fedor Petrov <petrovfedor1995@yandex.ru>
2023-09-21 16:29:06 +0400

    исправил mongoose

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+mongoose.createConnection('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
